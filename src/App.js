888d4639e112131ed3120f67b97cc5362f54ba0a
Samandar <samandar149official@gmail.com>
2022-12-26 17:34:03 +0500

    Testg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there!2</p>
+        <p>Hi there!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -14,7 +14,6 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-          1
         </a>
       </header>
     </div>
