9c724e13f64edae2bf38cd52edaa093ca3eb4b03
Samandar <samandar149official@gmail.com>
2023-01-06 12:21:01 +0500

    test change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hi there!</p>
-       <p>It's awesome!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>It's awesome!</p>
       </header>
     </div>
   );
